2c9db40df0198cdd6881b1b73b2e7a7c493f563a
studypg <studypg0925@yahoo.co.jp>
2018-07-31 11:22:58 +0900

    挨拶を追加その３

---
+++ Exec.java
@@ -0,0 +1,8 @@
+package example01;
+public class Exec{
+	public static void main(String[] args){
+		System.out.println("挨拶:おはよう");
+		System.out.println("挨拶:こんにちは");
+		System.out.println("挨拶:こんばんは");
+	}
+}
